a8dc2917652f1a89c9ca26674ef4330e530efc76
Tolu Alimi <tolualimi05@gmail.com>
2020-08-12 23:24:07 -0500

    Changed app.use to require(app)

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
