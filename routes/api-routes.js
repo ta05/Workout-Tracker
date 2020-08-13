2f885d10e3d536733ed1e4b34f5e1fda12f40ed3
Tolu Alimi <tolualimi05@gmail.com>
2020-08-12 23:22:19 -0500

    Fixed api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,41 @@
+const Workout = require("../models/workout.js");
+
+module.exports = function (app) {
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            });
+    });
+
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(dbWorkout => res.json(dbWorkout))
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    app.post("/api/workouts", ({ body }, res) => {
+        Workout.insertMany(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+}
